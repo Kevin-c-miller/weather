16c45b2f66584702b31a6f7431da095821645660
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-04-26 14:47:06 -0400

    created api call fucntions

--- src/services/apiConfig.ts
+++ src/services/apiConfig.ts
@@ -4,3 +4,27 @@ const KEY = process.env.REACT_APP_WEATHER_KEY;
 const url = 'https://api.openweathermap.org';
 
 console.log(KEY);
+
+// get weather
+export const getWeather = async (lat: string, long: string) => {
+  try {
+    const res = await axios.get(
+      `${url}/data/2.5/weather?lat=${lat}&lon=${long}&appid=${KEY}`
+    );
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// get more weather data (forecasts)
+export const getMoreWeather = async (lat: string, long: string) => {
+  try {
+    const res = await axios.get(
+      `${url}/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${KEY}`
+    );
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
