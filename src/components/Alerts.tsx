dc26a29b736d70ae3c95bcdadad674501b258831
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-04-29 18:33:57 -0400

    foxed typo

--- src/components/Alerts.tsx
+++ src/components/Alerts.tsx
@@ -29,7 +29,7 @@ const Alerts = ({ sender, event, description }: AlertsProps) => {
     setIsOpen(true);
   };
 
-  // cloase modal
+  // close modal
   const closeModal = () => {
     setIsOpen(false);
   };
